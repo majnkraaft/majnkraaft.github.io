72957bdf7a3e662225c46b18de8e184f8c84c4d9
majnkraaft <169532258+majnkraaft@users.noreply.github.com>
2025-05-19 20:05:46 +0300

    update timer.js

--- timer.js
+++ timer.js
@@ -20,7 +20,7 @@ function acceptCookies() {
         }
         
 const countdown = () => {
-            const countDate = new Date('Jun 23, 2025 13:00:00').getTime();
+            const countDate = new Date('Jun 20, 2025 13:00:00').getTime();
             const now = new Date().getTime();
             const gap = countDate - now;
 
